03a13a8c20cfde00ccbaf8e74d9f32c355439e03
Clemens Hemmerling <clemens@galileo.edu>
2018-07-15 19:56:31 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LugarPage } from '../pages/lugar/lugar';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { TabsPage } from '../pages/tabs/tabs';
+import { AboutPage } from '../pages/about/about';
+import { TerceraPage } from '../pages/tercera/tercera';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LugarPage,
+    PerfilPage,
+    TabsPage,
+    AboutPage,
+    TerceraPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +29,12 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LugarPage,
+    PerfilPage,
+    TabsPage,
+    AboutPage,
+    TerceraPage
   ],
   providers: [
     StatusBar,
